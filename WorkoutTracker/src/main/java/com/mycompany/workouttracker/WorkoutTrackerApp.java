7fca84f4451bb4b95ed4720646d95d03208df97a
Gurpreet Singh Gosal <er.gurpreetgosal@gmail.com>
2025-09-02 21:22:44 -0400

    Add source code folders for MovieLog and WorkoutTracker

---
+++ WorkoutTracker/src/main/java/com/mycompany/workouttracker/WorkoutTrackerApp.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.workouttracker;
+
+/**
+ *
+ * @author harpr
+ */
+public class WorkoutTrackerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Workout Tracker: ready");
+    }
+
+}
